e85df99cf775e774c770887f520f62cb02baee66
edgeboyo <mkolybko@gmail.com>
2021-11-11 01:52:10 +0000

    All GUI interactions completed

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 
+from datetime import date
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QApplication, QDialog, QHBoxLayout, QLabel, QPushButton, QTableWidget, QTableWidgetItem, QVBoxLayout
+from PyQt5.QtCore import QDate, QDateTime, QTimer
+from PyQt5.QtWidgets import QApplication, QDateTimeEdit, QDialog, QHBoxLayout, QLabel, QPushButton, QTableWidget, QTableWidgetItem, QVBoxLayout
 
 from diskpart import getDiskStructure, is_admin
 
@@ -93,6 +95,7 @@ class GUI(QDialog):
             self.partitionTable.setItem(i, 0, QTableWidgetItem(partEntry))
 
     def partTablePress(self, row, column):
+        self.clearActionLayout()
         self.part = self.disk.part[row]
 
         self.partitionLabel.setText(
@@ -101,11 +104,37 @@ class GUI(QDialog):
         self.setUpActionLayout()
 
     def setUpActionLayout(self):
-        pass
+        actionLabel = QLabel(
+            f"Selected Disk {self.disk.id} -> {self.part.shorthand()} for format")
+        self.actionTime = QDateTimeEdit(QDateTime.currentDateTime())
+        actionButton = QPushButton(f"Run scheduled format")
+        actionButton.clicked.connect(self.checkTimeAndRun)
+
+        self.actionLayout.addWidget(actionLabel)
+        self.actionLayout.addWidget(self.actionTime)
+        self.actionLayout.addWidget(actionButton)
+
+    def checkTimeAndRun(self):
+        dateTime = self.actionTime.dateTime()
+        currentTime = QDateTime.currentDateTime()
+
+        diff = currentTime.secsTo(dateTime)
+
+        if diff < 0:
+            print("Can't go back to the past")
+            return
+
+        self.diff = diff
+        self.awaitScreen()
 
     def clearActionLayout(self):
         for i in reversed(range(self.actionLayout.count())):
             self.actionLayout.itemAt(i).widget().deleteLater()
+
+    def disableInteractions(self):
+        self.refresh.setEnabled(False)
+        self.diskTable.setEnabled(False)
+        self.partitionTable.setEnabled(False)
 
     def resetGUIState(self):
         self.disks = None
@@ -120,12 +149,73 @@ class GUI(QDialog):
         self.diskTable.clear()
         self.diskTable.setColumnCount(0)
         self.diskTable.setRowCount(0)
+        self.diskTable.setEnabled(True)
 
         self.partitionTable.clear()
         self.partitionTable.setColumnCount(0)
         self.partitionTable.setRowCount(0)
+        self.partitionTable.setEnabled(True)
 
         self.clearActionLayout()
+
+    def awaitScreen(self):
+        self.clearActionLayout()
+        self.disableInteractions()
+        formatJob = QLabel(
+            f"Selected Disk {self.disk.id} -> {self.part.shorthand()} for format")
+        self.timeLabel = QLabel("Remaining to format job: N/A")
+
+        self.timer = QTimer(self)
+        self.timer.timeout.connect(self.advanceTime)
+        self.timer.start(1000)
+
+        timeStop = QPushButton("Stop job")
+        timeStop.clicked.connect(self.stopJob)
+
+        self.actionLayout.addWidget(formatJob)
+        self.actionLayout.addWidget(self.timeLabel)
+        self.actionLayout.addWidget(timeStop)
+
+    def advanceTime(self):
+        self.diff -= 1
+        if self.diff <= 0:
+            self.runScript()
+        diff = self.diff
+
+        secs = int(diff) % 60
+
+        diff /= 60
+        mins = int(diff) % 60
+
+        diff /= 60
+        hours = int(diff) % 60
+
+        diff /= 24
+        days = int(diff)
+
+        remaining = "Remaining to format job: %02d:%02d:%02d:%02d" % (
+            days, hours, mins, secs)
+
+        self.timeLabel.setText(remaining)
+
+    def stopJob(self):
+        self.timer.stop()
+        self.timer.deleteLater()
+        self.resetGUIState()
+
+    def runScript(self):
+        self.timer.stop()
+        self.timer.deleteLater()
+
+        self.clearActionLayout()
+
+        self.actionLayout.addWidget(QLabel("Formatting in progress..."))
+
+        # do formatting
+
+        # self.clearActionLayout()
+        # self.build()
+        # self.actionLayout.addWidget(QLabel("Formatting finished"))
 
 
 if __name__ == "__main__":
